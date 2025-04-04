9bf9f7127e698369fe6dad40b35e2e78b17f508a
Pedro Altoé Costa <pedroaltoecosta@gmail.com>
2025-04-04 09:48:39 +0100

    Initial Commit

---
+++ StoryLike/StoryLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryLikeApp.swift
+//  StoryLike
+//
+//  Created by Pedro Altoe Costa on 4/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoryLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
